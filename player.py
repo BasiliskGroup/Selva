1aca6c5694f27796449c9eaf98c3229e6a79ae9a
Isaac_Lagoy <isaacblagoy@gmail.com>
2025-02-21 09:01:33 -0600

    Added new type hinting system

--- player.py
+++ player.py
@@ -1,2 +1,8 @@
 from basilisk import Node
+from type_hints import Game
 
+class Player():
+    
+    def __init__(self, game: Game):
+        self.game = game
+        
---
+++ type_hints.py
@@ -0,0 +1,17 @@
+import basilisk as bsk
+from dataclasses import dataclass
+from typing import Callable
+
+@dataclass
+class Game():
+    engine: bsk.Engine
+    
+@dataclass
+class Player():
+    game: Game
+    
+@dataclass
+class HeldItem():
+    node: bsk.Node
+    down: Callable
+    up: Callable
