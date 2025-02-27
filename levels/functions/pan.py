4082e394a1649ce1d6d7f23f3576f2bb16124477
Isaac_Lagoy <isaacblagoy@gmail.com>
2025-02-26 19:12:33 -0600

    Added pans

---
+++ levels/functions/pan.py
@@ -0,0 +1,44 @@
+import glm
+import basilisk as bsk
+from typing import Callable
+from levels.interactable import Interactable
+
+def pan_loop(interact: Interactable, time: float=1, position: glm.vec3=None, rotation: glm.quat=None, loop_func: Callable=None) -> Callable:
+    """
+    Lerps camera to desires position and rotation and starts main loop with func.
+    Exits on pressing E.
+    Lerps camera back to player position
+    """
+    setattr(interact, 'camera', bsk.StaticCamera()) # position and rotation do not matter for init
+    setattr(interact, 'percent', 0)
+    setattr(interact, 'step', 1)
+    level = interact.level
+    game = level.game
+    
+    def func(dt: float) -> None:
+        # prepare variables for lerp
+        original_position = interact.camera.position = glm.vec3(*game.camera.position)
+        original_rotation = interact.camera.rotation = glm.quat(*game.camera.rotation)
+        final_position = glm.vec3(position) if position else original_position
+        final_rotation = glm.quat(rotation) if rotation else original_rotation
+        game.camera = interact.camera
+        interact.step = 1
+
+        # generate update function for the game
+        def update():
+            interact.percent = glm.clamp(interact.percent + game.engine.delta_time / time * interact.step, 0, 1)
+            game.camera.position = glm.mix(original_position, final_position, interact.percent)
+            game.camera.rotation = glm.slerp(original_rotation, final_rotation, interact.percent)
+
+            if interact.percent == 1 and game.key_down(bsk.pg.K_e): interact.step = -1 # initiate reverse lerp
+            if loop_func and interact.percent == 1 and interact.step == 1: loop_func()
+            if not (interact.percent == 0 and interact.step == -1): game.update = update # "recurse" through this function if not exiting
+            else: game.camera = game.player.camera # reapply the player camera when fully exited
+
+            level.update()
+            game.engine.update()
+        
+        # exit procedure for pan
+        game.update = update
+            
+    return func
--- levels/generators/bedroom.py
+++ levels/generators/bedroom.py
@@ -6,6 +6,7 @@ from levels.interactable import Interactable
 from helper.type_hints import Game
 from levels.functions.pickup import pickup_function, pickup_return_function
 from levels.functions.interpolate import lerp, lerp_interact, lerp_difference
+from levels.functions.pan import pan_loop
 
 
 def bedroom(game: Game) -> Level:
@@ -31,22 +32,14 @@ def bedroom(game: Game) -> Level:
 
 def animated_box(level: Level) -> Interactable:
     box = bsk.Node(
-        mesh = level.game.meshes['wheel_eight'],
-        material = level.game.materials['wheel_eight'],
+        mesh = level.game.meshes['john'],
+        scale = (0.25, 0.25, 0.25),
+        material = level.game.materials['john'],
         position = (0, 1.5, 0)
     )
     animated_box = Interactable(level, box)
     animated_box.active = pickup_function(animated_box, pickup_return_function(animated_box))
     return animated_box
-    
-def locked_box(level: Level) -> Interactable:
-    node = bsk.Node(
-        position = (3.5, 2.25, -4.35),
-        scale = (0.5, 0.5, 0.5),
-        mesh = level.game.meshes['box_three']
-    )
-    locked_box = Interactable(level, node)
-    return locked_box
     
 def key(level: Level) -> Interactable:
     node = bsk.Node(
@@ -70,3 +63,28 @@ def drawer(level: Level, position: glm.vec3) -> Interactable:
     drawer.passive = lerp_difference(drawer, time = 0.25, delta_position = (0, 0, 1))
     drawer.active = lerp_interact(drawer)
     return drawer
+
+def locked_box(level: Level) -> Interactable:
+    node = bsk.Node(
+        position = (3.5, 2.25, -4.35),
+        scale = (0.5, 0.5, 0.5),
+        mesh = level.game.meshes['box_three']
+    )
+    locked_box = Interactable(level, node)
+    
+    def active(dt: float) -> None:
+        """
+        1. Camera lerps to box
+        2. Player is allowed control (Moving wheels and exiting with e)
+        3. On exit, camera lerps to player position
+        """
+    
+    locked_box.active = pan_loop(
+        interact = locked_box, 
+        time = 0.5, 
+        position = node.position.data + glm.vec3(0, 0, 1.5),
+        rotation = glm.quat(),
+        loop_func = None
+    )
+    
+    return locked_box
