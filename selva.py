ea154a330448362182a66f10a736aa7456924ead
Isaac_Lagoy <isaacblagoy@gmail.com>
2025-04-23 19:52:21 -0500

    Minor boat things

--- selva.py
+++ selva.py
@@ -12,6 +12,7 @@ frame in bedroom safe - to where? (change safe code)
 improve fishing - fishing frames, blit instructions
 make pickup function look better (needs more scenes)
 add testing teleportation - add picture frame teleportation
+queue the player when picking up the picture frame
 """
 
 """
