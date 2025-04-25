bc8ea2ab6839f07de5dc9b01b77d3077937d23f6
Isaac_Lagoy <isaacblagoy@gmail.com>
2025-04-24 19:30:23 -0500

    changes

--- selva.py
+++ selva.py
@@ -11,11 +11,9 @@ while game.engine.running:
 frame in bedroom safe - to where? (change safe code)
 improve fishing - fishing frames, blit instructions
 make pickup function look better (needs more scenes)
-add testing teleportation - add picture frame teleportation
 queue the player when picking up the first picture frame
 add frame cycling with mouse scrolling
 crt on & teleport
-Maybe Fix portal delay
 increase wall width (Maybe jonah update depth test?)
 """
 
